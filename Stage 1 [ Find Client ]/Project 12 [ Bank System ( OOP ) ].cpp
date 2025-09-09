7cd1003ec1507d2c9dc0a9f679b583450f41e3eb
Ahmed Yaser Metwalli <ahmed.yaser.mt@gmail.com>
2025-09-09 19:54:07 +0300

    Add files via upload

---
+++ Stage 1 [ Find Client ]/Project 12 [ Bank System ( OOP ) ].cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "clsBankClient.h"
+
+using namespace std;
+
+int main()
+{
+    clsBankClient Client1 = clsBankClient::Find("A101");
+    Client1.Print();
+
+    clsBankClient Client2 = clsBankClient::Find("A101", "1234");
+    Client2.Print();
+
+    cout << clsBankClient::IsClientExist("A1011");
+
+    system("pause>0");
+    return 0;
+}
+
