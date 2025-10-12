d157f25acab821b2a39784cf51185d651b7c313a
Ahmed Yaser Metwalli <ahmed.yaser.mt@gmail.com>
2025-10-12 21:46:43 +0300

    Add files via upload

---
+++ Stage 10 [ Login & Permissions ( Login ) ]/Project 12 [ Bank System ( OOP ) ].cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+
+using namespace std;
+
+int main()
+{
+    // clsMainScreen::ShowMainMenue();
+    clsLoginScreen::ShowLoginScreen();
+
+    system("pause>0");
+    return 0;
+}
+
